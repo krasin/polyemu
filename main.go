155130c7ac2af2f772ef1e0b3d47c3c31b9b3bcc
Ivan Krasin <imkrasin@gmail.com>
2012-07-30 05:14:07 +0000

    Use SET X, 1 as a default program

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 func main() {
 	e := &dcpu16.Emulator{}
 	st := &emu.State{
-		Mem: make([]byte, 0), //128*1024),
+		Mem: make([]byte, 128*1024),
 		Reg: make([]uint64, 32),
 	}
+	// SET X, 1
+	st.Mem[0] = 0x61
+	st.Mem[1] = 0x84
 	if _, code := e.Step(st); code != emu.OK {
 		fmt.Printf("code = %v\n", code)
 	}

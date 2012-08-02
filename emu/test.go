8c302acf88c88b01df786f870559518307f14c98
Ivan Krasin <imkrasin@gmail.com>
2012-08-02 01:56:03 +0000

    Implement the first auto test for dcpu16 (SET X, 1). Currently, it's inconvenient to specify the data and hard to interpret the result

---
+++ emu/test.go
@@ -0,0 +1,36 @@
+package emu
+
+type T interface {
+	Errorf(fmt string, args ...interface{})
+}
+
+type Test struct {
+	Mem     []byte
+	Reg     []uint64
+	WantReg []uint64
+	N       int
+}
+
+func RunTest(t T, e Emulator, test Test) {
+	st := &State{
+		Mem: make([]byte, len(test.Mem)),
+		Reg: make([]uint64, len(test.Reg)),
+	}
+	copy(st.Mem, test.Mem)
+	copy(st.Reg, test.Reg)
+	for i := 0; i < test.N; i++ {
+		if _, code := e.Step(st); code != OK {
+			t.Errorf("Execution failed at step %d with the following code: %v\n", i, code)
+			return
+		}
+	}
+	for i, v := range st.Reg {
+		if i >= len(test.WantReg) {
+			break
+		}
+		want := test.WantReg[i]
+		if v != want {
+			t.Errorf("Reg[%d]: want 0x%x, got 0x%x\n", i, want, v)
+		}
+	}
+}

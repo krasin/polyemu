5e9b681847c8ee6e25d977d8d938c05680d23fc4
Ivan Krasin <imkrasin@gmail.com>
2012-08-27 04:23:20 +0000

    Delete build_lala.sh

--- arch/msp430/tools/build_lala.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Compiles asm snippet and disassembles it.
-# Useful for preparing binary tests.
-# Usage: ./tools/build_snippet.sh
-set -eu
-
-msp430-gcc -c -o asm-snippet tools/asm-snippet.s
-msp430-objdump -D asm-snippet
